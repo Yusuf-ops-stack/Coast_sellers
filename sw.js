ce9f702d3d6d610d461651bc15224ec7a9df58f4
Yusuf-ops-stack <2505joemwandoro@gmail.com>
2026-04-10 18:35:14 +0300

    first commit

--- sw.js
+++ sw.js
@@ -1,18 +1,42 @@
+const CACHE_NAME = "coastmall-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("coastmall-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (OFFLINE SUPPORT)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
